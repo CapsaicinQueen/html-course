c94e8a38e78d2dce55e4de003e31948765af2487
Briana Paquin <brianapaquin@gmail.com>
2020-07-01 16:18:10 -0400

    resume changed to php

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+    
+    <title>Briana Paquin | Resume</title>
+
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  </head>
+  
+    <body>
+    <br/><br/>
+
+    <nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Briana Paquin</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        
+        
+      </ul>
+     
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="personal_statement.html">Personal Statement</a></li>
+        
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+  
+  <!-- Experience -->
+  <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title"><strong>Experience</strong></h3>
+        </div>
+          <div class="panel-body">
+            <div class="col-md-8"><strong>An Unlikely Story</strong>
+          
+              <P align="justify">barista, customer service, kitchen help</P>
+            </div>
+              <div class="col-md-4" align="right">
+                <em>May 2019 - Present</em>
+              </div>
+          </div>
+
+<div class="panel-body">
+            <div class="col-md-8"><strong>Madonna Manor</strong>
+             
+              <P align="justify">dietary aide</P>
+            </div>
+              <div class="col-md-4" align="right">
+                <em>March 2019 - August 2019</em>
+              </div>
+          </div>
+
+<div class="panel-body">
+            <div class="col-md-8"><strong>Calvin Klein</strong>
+             
+              <P align="justify">sales associate</P>
+            </div>
+              <div class="col-md-4" align="right">
+                <em>May 2018 - January 2019</em>
+              </div>
+          </div>
+
+<div class="panel-body">
+            <div class="col-md-8"><strong>Natural Lawn of America</strong>
+             
+              <P align="justify">customer service representative</P>
+            </div>
+              <div class="col-md-4" align="right">
+                <em>August 2017 - May 2018</em>
+              </div>
+          </div>
+
+<div class="panel-body">
+            <div class="col-md-8"><strong>Chic 2 Chic Consignments</strong>
+              
+              <P align="justify">customer service representative</P>
+            </div>
+              <div class="col-md-4" align="right">
+                <em>August 2016 - July 2017</em>
+              </div>
+          </div>
+
+  </div>
+
+    <br/><br/>
+
+
+
+  <!-- Education -->
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title"><strong>Education</strong></h3>
+      </div>
+        <div class="panel-body">
+          <div class="col-md-2"><img src="Images/Irish Tree.jpg" height="50" </div>
+          </div>
+          <div class="col-md-4">Salem State University
+          <P align="justify">Bachelor of Arts (BA)</P>
+          <P align="justify">Music with a Theatre Minor</P>
+          <P align="justify">GPA: 3.8</P></div>
+          <div class="col-md-6" align="right">2012</div>
+        </div>
+      </div>
+
+    </div>
+
+
+
+
+
+       <br/><br/>
+
+<div class="container">
+
+  <!-- Certifications -->
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title"><strong>Certifications</strong></h3>
+      </div>
+        <div class="panel-body">
+          <div class="col-md-4">Basic html</div>
+             <P align="right">GoSkills - 2020</P>
+          <div class="col-md-4">Basic Excel</div>
+             <P align="right">GoSkills - 2020</P>
+          <div class="col-md-4">Bartender's Certification</div>
+             <P align="right">Professional School of Bartending - Providence, Rhode Island - 2012</P>
+         </div>
+        </div>
+
+      </div>
+    </div>
+
+
+      <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+  </body>
+
+  
+
+
+       <br/><br/>
+
+
+
+    	<footer>
+      <p align="center">&copy; Briana Paquin -all rights reserved</p>
+      </footer>
+
+
+
+
+
+</html>
